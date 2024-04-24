c7bdae1c0c7ec8818024a33d8df8f47fafab0081
dawitgetuu <archfun3@gmail.com>
2024-04-24 12:23:59 -0700

    web_flask

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,39 +1,30 @@
 #!/usr/bin/python3
-'''A simple Flask web application.
-'''
+"""
+    Sript that starts a Flask web application
+"""
 from flask import Flask, render_template
-
 from models import storage
-from models.amenity import Amenity
-from models.state import State
-
-
+import os
 app = Flask(__name__)
-'''The Flask application instance.'''
-app.url_map.strict_slashes = False
 
 
-@app.route('/hbnb_filters')
-def hbnb_filters():
-    '''The hbnb_filters page.'''
-    all_states = list(storage.all(State).values())
-    amenities = list(storage.all(Amenity).values())
-    all_states.sort(key=lambda x: x.name)
-    amenities.sort(key=lambda x: x.name)
-    for state in all_states:
-        state.cities.sort(key=lambda x: x.name)
-    ctxt = {
-        'states': all_states,
-        'amenities': amenities
-    }
-    return render_template('10-hbnb_filters.html', **ctxt)
-
-
-@app.teardown_appcontext
-def flask_teardown(exc):
-    '''The Flask app/request context end event listener.'''
+def handle_teardown(self):
+    """
+        method to handle teardown
+    """
     storage.close()
 
 
+@app.route('/hbnb_filters', strict_slashes=False)
+def filters_list():
+    """
+        method to display html page 6-index.html
+    """
+    states = storage.all('State').values()
+    amenities = storage.all('Amenity').values()
+    return render_template(
+        "10-hbnb_filters.html",
+        states=states, amenities=amenities)
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=5000)
